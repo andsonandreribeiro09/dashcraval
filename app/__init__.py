79e3dbec5beeef061507f968e9891ccd4f52d15a
Andson Andre <81991135+andsonandreribeiro09@users.noreply.github.com>
2024-11-19 14:31:36 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -695,5 +695,7 @@ def index():
     return render_template('home.html')  # Página inicial gerenciada pelo Flask
 
 
+# Executando a aplicação
 if __name__ == '__main__':
+    server.run(server=server)
     
